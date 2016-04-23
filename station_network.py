a1676316a83d2eb1c8df8074ceb882eb2c4eb9a5
Maximilian Remming <maxremming@gmail.com>
2016-04-23 20:56:03 +0300

    Added station class

--- station_network.py
+++ station_network.py
@@ -13,19 +13,39 @@ class StationNetwork:
         rows = cursor.fetchall()
 
         for i in rows:
-            fr, to = i[0], "VKM" if "VKM" in i[1] else i[1]
+            fr, to = PumpStation(i[0]), PumpStation("VKM") if "VKM" in i[1] else PumpStation(i[1])
             self.stations.add_edge(fr, to)
 
     def neighbors(self, node, depth, visited=[]):
         if depth >= 0:
-            yield node
+            for i in self.stations:
+                if node == i:
+                    yield i
+                    break
         visited.append(node)
         for i in nx.all_neighbors(self.stations, node):
             if i not in visited:
                 for j in self.neighbors(i, depth-1, visited):
                     yield j
 
+class PumpStation:
+    def __init__(self, code):
+        self.code = code
+
+    def __str__(self):
+        return "Pump station:\n\tCode: " + str(self.code)
+
+    def __hash__(self):
+        return hash(self.code)
+
+    def __eq__(self, other):
+        if type(other) == str:
+            return self.code == other
+        return self.code == other.code
+
 if __name__ == "__main__":
     import networkx.drawing.nx_pydot as pydot
     a = StationNetwork()
-    pydot.to_pydot(a.stations).write_png("hi.png", prog="fdp")
+    for i in a.neighbors("1078", 1):
+        print(i)
+    #pydot.to_pydot(a.stations).write_png("hi.png", prog="fdp")
