638807102ad96f699715fdc49ece45606a45b981
Paul Saikko <paul.saikko@cs.helsinki.fi>
2016-04-23 13:16:00 +0300

    .

--- read_geotiffs.py
+++ read_geotiffs.py
@@ -1,81 +1,28 @@
 #!/usr/bin/python3
 
-from PIL import Image  # python3-pil
-import numpy  # python3-numpy
-import os
-from osgeo import osr, gdal, gdalconst  # python3-gdal
-from geopy.distance import GreatCircleDistance as coord_dist  # python3-geopy
+from PIL import Image, ImageDraw # python3-pil
+import numpy # python3-numpy
+import os 
+import shapefile # python3-pyshp
+from osgeo import osr, gdal, gdalconst # python3-gdal
+from geopy.distance import GreatCircleDistance as coord_dist # python3-geopy
 
-GEOTIFF_DIR = 'IL sadedata 2015'
+from helpers import *
 
+GEOTIFF_DIR = '../hackathon/IL sadedata 2015'
+SHAPEFILE_DIR = '../hackathon/Pumppaamoalue-shape'
 
-# The following method translates given latitude/longitude pairs into pixel locations on a given GEOTIF
-# INPUTS: geotifAddr - The file location of the GEOTIF
-#      latLonPairs - The decimal lat/lon pairings to be translated in the form [[lat1,lon1],[lat2,lon2]]
-# OUTPUT: The pixel translation of the lat/lon pairings in the form [[x1,y1],[x2,y2]]
-# NOTE:   This method does not take into account pixel size and assumes a high enough 
-#	  image resolution for pixel size to be insignificant
-def latLonToPixel(geotifAddr, latLonPairs):
-    # Load the image dataset
-    ds = gdal.Open(geotifAddr)
-    # Get a geo-transform of the dataset
-    gt = ds.GetGeoTransform()
-    # Create a spatial reference object for the dataset
-    srs = osr.SpatialReference()
-    srs.ImportFromWkt(ds.GetProjection())
-    # Set up the coordinate transformation object
-    srsLatLong = srs.CloneGeogCS()
-    ct = osr.CoordinateTransformation(srsLatLong, srs)
-    # Go through all the point pairs and translate them to latitude/longitude pairings
-    pixelPairs = []
-    for point in latLonPairs:
-        # Change the point locations into the GeoTransform space
-        (point[1], point[0], holder) = ct.TransformPoint(point[1], point[0])
-        # Translate the x and y coordinates into pixel values
-        x = (point[1] - gt[0]) / gt[1]
-        y = (point[0] - gt[3]) / gt[5]
-        # Add the point to our return array
-        pixelPairs.append([int(x), int(y)])
-    return pixelPairs
+myshp = open(os.path.join(SHAPEFILE_DIR,"Alue.shp"), "rb")
+mydbf = open(os.path.join(SHAPEFILE_DIR,"Alue.dbf"), "rb")
+r = shapefile.Reader(shp=myshp, dbf=mydbf)
+shapes = r.shapes()
 
+shapeCoords = []
 
-# The following method translates given pixel locations into latitude/longitude locations on a given GEOTIF
-# INPUTS: geotifAddr - The file location of the GEOTIF
-#      pixelPairs - The pixel pairings to be translated in the form [[x1,y1],[x2,y2]]
-# OUTPUT: The lat/lon translation of the pixel pairings in the form [[lat1,lon1],[lat2,lon2]]
-# NOTE:   This method does not take into account pixel size and assumes a high enough 
-#	  image resolution for pixel size to be insignificant
-def pixelToLatLon(geotifAddr, pixelPairs):
-    # Load the image dataset
-    ds = gdal.Open(geotifAddr)
-    # Get a geo-transform of the dataset
-    gt = ds.GetGeoTransform()
-    # Create a spatial reference object for the dataset
-    srs = osr.SpatialReference()
-    srs.ImportFromWkt(ds.GetProjection())
-    # Set up the coordinate transformation object
-    srsLatLong = srs.CloneGeogCS()
-    ct = osr.CoordinateTransformation(srs, srsLatLong)
-    # Go through all the point pairs and translate them to pixel pairings
-    latLonPairs = []
-    for point in pixelPairs:
-        # Translate the pixel pairs into untranslated points
-        ulon = point[0] * gt[1] + gt[0]
-        ulat = point[1] * gt[5] + gt[3]
-        # Transform the points to the space
-        (lon, lat, holder) = ct.TransformPoint(ulon, ulat)
-        # Add the point to our return array
-        latLonPairs.append([lat, lon])
+for s in shapes:
+	shapeCoords.append([[pt[1]/100000, pt[0]/1000000] for pt in s.points])
 
-    return latLonPairs
-
-
-def enumfiles(dpath):
-    for path, subdirs, files in os.walk(dpath):
-        for name in files:
-            yield os.path.join(path, name)
-    return
-
+print([shapeCoords[0][0]])
 
 cumulative_values = None
 
@@ -94,11 +41,14 @@ topleftc = pixelToLatLon(files[0], [[0, 0]])
 toprightc = pixelToLatLon(files[0], [[W, 0]])
 botrightc = pixelToLatLon(files[0], [[W, H]])
 
+print(topleftc)
+print(botrightc)
+
 print("geotiff dimensions: %dx%d" % (W, H))
 print("area width:", coord_dist(topleftc, toprightc))
 print("area height:", coord_dist(toprightc, botrightc))
 
-N = 1000
+N = 100
 i = 0
 
 for filepath in files:
@@ -120,4 +70,5 @@ avg = numpy.array(numpy.round(avg), dtype=numpy.uint8)
 
 # show average of first N radar images
 res = Image.fromarray(avg)
-res.show();
+res.show()
+
