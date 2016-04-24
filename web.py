b2501cba31164ae725b365aa6399c36212701cad
Maximilian Remming <maxremming@gmail.com>
2016-04-24 12:02:10 +0300

    Changed how avg works

--- web.py
+++ web.py
@@ -13,13 +13,14 @@ def index():
 def avg(code, start_date, end_date):
     filename = "JVP" + code + "_" + start_date + "_" + end_date + ".png"
     from os import path
-    if not path.exists("tmp/" + filename):
+    if not path.exists("static/tmp/" + filename):
         try:
             p = make_plot("JVP" + code, datetime.datetime.strptime(start_date, "%Y-%m-%d"), datetime.datetime.strptime(end_date, "%Y-%m-%d"))
-            p.savefig("tmp/" + filename, dpi=300)
+            p.savefig("static/tmp/" + filename, dpi=300)
         except:
             abort(404)
-    return send_file("tmp/" + filename, mimetype='image/png')
+    return "static/tmp/" + filename
+    # return send_file("tmp/" + filename, mimetype='image/png')
 
 if __name__ == "__main__":
     app.run()
